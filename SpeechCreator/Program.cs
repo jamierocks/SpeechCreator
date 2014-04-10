4276ab9ac74ee3c3bbdea5840a541514c18be8ab
Jamie <jamieflargm@icloud.com>
2014-04-10 21:20:45 +0100

    SpeechCreation is officially easy, and you can parse data in through java.

---
+++ SpeechCreator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechCreator
+{
+    class Program
+    {
+
+        static SpeechSynthesizer speaker = new SpeechSynthesizer();
+
+        static void Main(string[] args)
+        {
+
+            string say = args[0];
+
+            speaker.Speak(say);
+
+        }
+
+    }
+}
